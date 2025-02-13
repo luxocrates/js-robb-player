2a5e3a686cecf8ec11f9c38630c59ccbd4ea58ad
Mike Dean <3223304+luxocrates@users.noreply.github.com>
2025-02-12 23:44:16 -0800

    Fix Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/js-robb-player",
   server: {
     // Hot module reload
     hmr: false,
